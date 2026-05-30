8584a62672a3b732415f049a8c3cee3563764daf
j2 <j2@mac-book-pro-joan.local>
2026-05-30 21:05:34 +0200

    botón papelera con svg

--- Class/UserInterface.js
+++ Class/UserInterface.js
@@ -36,8 +36,18 @@ createNotesContainer(){
 
 renderNotesList(notesList){
 
+    //borramos todo el contenedor de notas
     this.notesContainer.innerHTML="";
 
+    //html del icono de la papelera vectorial "svg" que usaremos con el metodo innerHTML para reemplazar el boton "standard" del navegador.
+    const btnDelete = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 
+  10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 
+  3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 
+  4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/></svg>`
+
+
+    //renderizamos cada nota dentro del array recibido en noteList.
      notesList.forEach(note => {
 
         const noteCard = document.createElement("div");
@@ -56,7 +66,7 @@ renderNotesList(notesList){
         const deleteButton = document.createElement("button");
         deleteButton.classList.add("delete-button");
         deleteButton.dataset.deleteButton = "";
-        deleteButton.textContent = "Delete";
+        deleteButton.innerHTML = btnDelete;
       
         titleContainer.append(noteDateCreated);
         titleContainer.append(noteTitle);
@@ -65,7 +75,7 @@ renderNotesList(notesList){
         noteCard.append(deleteButton);
 
         this.notesContainer.append(noteCard)
-
+    
     });
     }    
 
